3978a4d816b5e255c69aa0ebde498083eb45c21a
gerryli0214 <957282514@qq.com>
2019-03-25 15:35:49 +0800

    Add files via upload
    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    apps: [{
+        name: 'API',
+        script: 'app.js',
+
+        // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+        args: 'one two',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        output: './logs/out.log',
+        error: './logs/error.log',
+        log: './logs/combined.outerr.log',
+        log_date_format: 'YYYY-MM-DD HH：mm: ss', //日志时间戳格式
+        max_memory_restart: '1G',
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }],
+
+    deploy: {
+        production: {
+            user: 'node',
+            host: '212.83.163.1',
+            ref: 'origin/master',
+            repo: 'git@github.com:repo.git',
+            path: '/var/www/production',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+        }
+    }
+};
