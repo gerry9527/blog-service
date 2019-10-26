bde92566cb40a6eb84f7b8bf916145aabdb76a4e
gerry <957282514@qq.com>
2019-10-26 22:12:08 +0800

    支持history模式

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const users = require('./router/user.js');
 const index = require('./router/index');
 const db = require('./db.config');
 const bodyParser = require("body-parser");
+const history = require('connect-history-api-fallback')
 
 app.use(logger(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
 app.use(cookieParse());
 app.use('/', index);
 app.use('/users', users);
 app.use(bodyParser.json());
+// 支持history模式
+app.use(history())
 //静态资源文件
 app.use(express.static(`${__dirname}/public`));
 
